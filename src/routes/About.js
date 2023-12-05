93c2f5520b0aebd2a1b28c0f6bb4912af54e20ce
MatShadow9 <razorteeth1223@gmail.com>
2023-12-04 22:32:19 -0500

    end of November changes

--- src/routes/About.js
+++ src/routes/About.js
@@ -5,6 +5,7 @@ function About() {
     return (
         <div>
             <h1 className='animate__animated animate__bounceIn'>This is the about page</h1>
+            <p>(insert blurb about who Steven and Ellen Hartfield are, what their goals are for Kenya, and what they hope to accomplish through this website.)</p>
         </div>
     )
 }
