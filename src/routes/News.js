6245012883b27b7f3522a4b7d655fd369cf35b07
MatShadow9 <razorteeth1223@gmail.com>
2023-12-18 22:39:02 -0500

    updated buttons, submit/clear buttons fully work now, added image upload button

--- src/routes/News.js
+++ src/routes/News.js
@@ -7,14 +7,15 @@ function News({ user }) {
     const [completePost, setCompletePost] = useState({});
     const [updatePosts, setUpdatePosts] = useState(['']);
     const [newsData, setNewsData] = useState();
-    const [newsTitle, setNewsTitle] = useState(['']);
-    const [newsConfig, setNewsConfig] = useState();
+    const [newsTitle, setNewsTitle] = useState('');
+    const [newsConfig, setNewsConfig] = useState('1');
     const submit = async (e) => {
         //axios.post
         const newPost = { ...completePost };
         newPost['title'] = newsTitle;
         newPost['date'] = moment().format();
         newPost['paragraphs'] = updatePosts;
+        newPost['configuration'] = newsConfig;
         setCompletePost(newPost);
         // console.log(setUpdateDate, setUpdatePosts);
         try {
@@ -26,7 +27,9 @@ function News({ user }) {
     }
 
     const resetButton = (f) => {
-
+        setUpdatePosts([''])
+        setNewsTitle('')
+        setNewsConfig('1')
     }
 
 
@@ -42,48 +45,6 @@ function News({ user }) {
         fetchData();
     }, [])
 
-    //pretend data from a database
-    // display the title and paragraphs and can put in a different src for the image for now
-    // configuration right now just defines how the layout looks
-    // image: 'left' means image on the left and paragraph on the right
-    // image: 'right' means image on the right and paragraph on the left
-    // image: 'main' means image takes up entire row
-    /*const mockData = {
-        entries: [
-            {
-                title: 'First Title',
-                paragraphs: [
-                    'First entry first paragraph',
-                    'First entry second paragraph'
-                ],
-                image: require('../assets/funny_stock_image.jpg'),
-                configuration: {
-                    image: 'left',
-                    animateClassLeft: 'bounceIn',
-                    animateClassRight: 'bounceIn'
-                }
-            },
-            {
-                title: 'Second Title',
-                paragraphs: [
-                    'Second entry first paragraph',
-                    'Second entry second paragraph'
-                ],
-                image: require('../assets/funny_stock_image2.jpg'),
-                configuration: {
-                    image: 'right'
-                }
-            },
-            {
-                title: 'Image of Event',
-                paragraphs: [],
-                image: require('../assets/funny_stock_image3.jpg'),
-                configuration: {
-                    image: 'only'
-                }
-            },
-        ]
-    } */
     const addTextArea = () => {
         const newPosts = updatePosts.concat();
         newPosts.push('');
@@ -102,12 +63,6 @@ function News({ user }) {
         setUpdatePosts(newPosts);
     }
 
-    const changeTitle = (value, idx) => {
-        const newPosts = newsTitle.concat();
-        newPosts[idx] = value;
-        setNewsTitle(newPosts);
-    }
-
     return (
         <div>
             <h1>Hartfield Mission News</h1>
@@ -115,9 +70,7 @@ function News({ user }) {
             <br></br>
             {user && user.email && (
                 <>
-                    {newsTitle.map((e, idx) => (
-                        <input name="newTitle" key={'e ' + idx} type="text" placeholder="Title" value={e} onChange={t => changeTitle(t.target.value, idx)}></input>
-                    ))}
+                    <input name="newTitle" key={'newsTitle'} type="text" placeholder="Title" value={newsTitle} onChange={e => setNewsTitle(e.target.value)}/>
                     <br></br>
                     {updatePosts.map((p, idx) => (
                         <><textarea name="updatePost" key={'p ' + idx}
@@ -129,23 +82,25 @@ function News({ user }) {
                     ))}
                     <br></br>
                     <label>Config
-                        <select value={newsConfig} onChange={setNewsConfig}>
+                        <select value={newsConfig} onChange={e => setNewsConfig(e.target.value)}>
                             <option value="1">Left-Sided</option>
                             <option value="2">Right-Sided</option>
                             <option value="3">Picture Only</option>
                         </select>
                     </label>
-                    <button onClick={addTextArea}>Add Paragraph</button>
+                    <button className="btn btn-primary" onClick={addTextArea}>Add Paragraph</button>
                     <br></br>
-                    <button type="submit" onClick={submit}>Post</button>
-                    <button type="reset" onClick={resetButton}>Clear</button>
+                    <button type="button">Upload Image</button>
+                    <br></br>
+                    <button type="submit" className="btn btn-success" onClick={submit}>Post</button>
+                    <button type="reset" className="btn btn-danger" onClick={resetButton}>Clear</button>
                 </>
             )}
             <div style={{ margin: "0 10%" }}>
                 {newsData && newsData.map((g) => (
                     <p>{g.title}<br></br>
-                        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: g.configuration.image === "right" ? "row" : "row-reverse" }}>
-                            <div style={{ margin: "0 10%", width: "200px", display: g.configuration.image === "only" ? "none" : "block" }}>{g.paragraphs.map((p) => (<p>{p}</p>))}</div>
+                        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: g.configuration === "2" ? "row" : "row-reverse" }}>
+                            <div style={{ margin: "0 10%", width: "200px", display: g.configuration === "3" ? "none" : "block" }}>{g.paragraphs.map((p) => (<p>{p}</p>))}</div>
                             <div style={{ margin: "0 10%", height: "200px", width: "200px" }}><img src={g.image} width="100%" height="100%"></img></div></div></p>
                 )
                 )}
